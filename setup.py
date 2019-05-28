43a276262eba584a143b5b159244887a91ab3305
j-veylop <velezlopezjuan1@gmail.com>
2019-05-28 18:22:34 +0200

    directories working now

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
-print('Introduce number of employees')
-num_emplo = input()
-print('Introduce names of employees')
-for i in range(0, num_emplo - 1):
-  name_emplo[i] = input
+import os
+
+# We create a folder to save the scripts in
+os.mkdir(".scripts")
+
+# And another one to keep logs and tables
+os.mkdir(".punch-control")
+
+# Now we need the list of employees to track
+employees = input("Enter a list of employees separated by spaces ")
+list = employees.split()
+
+# With this loop we generate the folders for each employee
+for i in list:
+    os.mkdir("/home/pi/control_test/"+i)
+
+
