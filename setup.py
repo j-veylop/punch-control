91288e59196578f441c644bf0aef823f8ae6dec8
j-veylop <velezlopezjuan1@gmail.com>
2019-05-28 08:40:53 +0200

    added python script

---
+++ setup.py
@@ -0,0 +1,5 @@
+print('Introduce number of employees')
+num_emplo = input()
+print('Introduce names of employees')
+for i in range(0, num_emplo - 1):
+  name_emplo[i] = input
--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
-declare -a NAME_EMPLOYEE
 echo 'Number of employees'
 read NUMBER_EMPLOYEES
 echo 'Write each name of the employees, pressing enter after each one'
 for i in {1..$NUMBER_EMPLOYEES}
 do
-read NAME
-NAME_EMPLOYEE[$i]=NAME
-echo $NAME_EMPLOYEE
+read NAME[$i]
+NAME_EMPLOYEE[$i]="$NAME"
+echo ${NAME_EMPLOYEE[@]}
 done
