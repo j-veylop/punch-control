5dc837e8e7f7bd8fce02755c5fc4d604ef2df7ae
j-veylop <velezlopezjuan1@gmail.com>
2019-05-29 16:41:56 +0200

    code cleanup

--- test.py
+++ test.py
@@ -7,10 +7,18 @@ import os
 #os.mkdir(".punch-control")
 
 employees = []
+mac_address = []
 name = ""
 num_employees = int(input("Enter number of employees: "))
 
+print("Introduce the names of the employees")
+
 for i in range(0, num_employees):
-    name = input("Enter name of employees one by one and press enter ")
+    name = input("")
     employees.append(name)
+
+for i in range(0, num_employees):
+    mac = input("Enter mac address for " +employees[i] +" " )
+    mac_address.append(mac)
 print(employees)
+print(mac_address)
